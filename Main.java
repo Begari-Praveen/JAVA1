fe3c334c3849ac67b44c09c37e36123e46b8832d
Praveen <praveenbegari09@gmail.com>
2026-05-18 09:41:48 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,151 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class LinkedList{
+    class Node{
+        int data;
+        Node next ;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    Node head = null;
+    Node tail = null;
+    
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode.next = head ;
+        head = newNode;
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void add(int pos , int data){
+        Node newNode = new Node(data);
+        if(pos < 0){
+            System.out.println("not possible");
+            return;
+        }
+        if(pos == 0){
+            addFirst(data);
+            return;
+        }
+        Node temp = head;
+        int i=0;
+        while(i<pos-1 && temp != null){
+            temp=temp.next;
+            i++;
+        }
+       if(temp == null){
+           System.out.println("out of range ");
+           return;
+       }
+      newNode.next = temp.next;
+      temp.next = newNode;
+      
+      if(newNode.next == null){
+          tail = newNode;
+      }
+    }
+    public void deleteFirst(){
+        if(head == null){
+          System.out.println("list is empty");
+          return;
+        }
+        if(head == tail){
+            System.out.println(head.data + " deleted");
+            head = null;
+            tail = null;
+            return;
+        }
+        System.out.println(head.data + "deleted");
+        head = head.next;
+    }
+    
+    public void deleteLast(){
+         if(head == null){
+          System.out.println("list is empty");
+          return;
+        }
+        if(head == tail){
+            System.out.println(head.data + " deleted");
+            head = null;
+            tail = null;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != tail){
+            temp = temp.next;
+        }
+        temp.next = null;
+        tail = temp;
+    }
+    public void delete(int pos){
+         if(pos<0){
+          System.out.println("Invalid");
+          return;
+        }
+        if(head == null){
+            System.out.println("list is empty");
+          return;
+        }
+        if(pos == 0){
+            deleteFirst();
+        }
+        int i=0;
+        Node temp = head ;
+        while(i<pos-1 && temp != null){
+            temp = temp.next ;
+            i++;
+        }
+        if(temp== null || temp.next == null ){
+            System.out.println("out of range");
+            return;
+        }
+        if(temp.next ==tail){
+            temp.next = null;
+            tail = temp;
+            return;
+        }
+        System.out.println(temp.next.data+" deleted");
+        temp.next = temp.next.next;
+        
+    }
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+         LinkedList list = new LinkedList();
+         list.addFirst(10);
+         list.addLast(20);
+         list.addLast(30);
+         list.addLast(40);
+         list.addLast(50);
+         list.add(2,15);
+         list.print();
+         list.deleteFirst();
+         list.deleteLast();
+         list.delete(2);
+         list.print();
+        
+    }
+}
