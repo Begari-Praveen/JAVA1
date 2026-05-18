8adade121b3037b39f605d5bab3206aeaf889d9d
Praveen <praveenbegari09@gmail.com>
2026-05-18 10:56:48 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -178,6 +178,19 @@ class LinkedList{
         }
         return true;
     }
+    public boolean detectCycle(){
+        Node slow = head;
+        Node fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast= fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
     
     
 }
